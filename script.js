10826d9891a1821defe07608af3b076d96f50321
George Nichols <georgehniii@gmail.com>
2022-04-14 17:17:59 -0700

    Red and black still go through on ranomColors

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// Your JS goes here
+const body = document.body;
+var color = '';
+var selector = "rb";
+for(var i = 1; i <= 81; i++){
+    let box = document.createElement('div');
+    box.classList.add('checker');
+    body.appendChild(box);
+ 
+    if (i%2 !== 0){
+        color = "red";
+    }else{
+        color = "black";
+    }
+    box.style.backgroundColor = color;
+    var perc = "11.1%";   
+    box.style.width = perc;
+    box.style.height = "11.1vw";
+    box.style.paddingBottem = perc;
+    box.style.float = "left";
+}
+console.log(body);
+randomColors();
+
+function randomColors(){
+    const randomize = document.querySelectorAll('.checker');
+    [].forEach.call(randomize, (e)=>{
+        e.style.backgroundColor = "#" + Math.floor(Math.random()*16777215);
+       });
+}
